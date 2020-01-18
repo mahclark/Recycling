bb1fd8eb2b13dedd4a2f72af29932cac169724ba
mahclark <mc2124@cam.ac.uk>
2020-01-18 18:33:45 +0000

    Added Location, including Google API. Added query by material functionality.

--- src/Banks.java
+++ src/Banks.java
@@ -1,41 +1,38 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Banks {
 
-    Set<Bank> bankSet = new HashSet<>();
+    private Set<Bank> bankSet = new HashSet<>();
+    private List<String> materials; //A list of all materials in the data
+    private final int dataOffset = 3;  //number of csv items before recycling information starts
 
-    Banks(String path) {
-        BufferedReader br = null;
+    public Banks(String path) {
+        BufferedReader reader = null;
         String line;
 
         try {
             int bankCount = 0;
-            String[] materials = new String[0];
 
-            br = new BufferedReader(new FileReader(path));
-            while ((line = br.readLine()) != null) {
+            reader = new BufferedReader(new FileReader(path));
+            while ((line = reader.readLine()) != null) {
 
                 String[] bankData = line.split(",");
 
                 if (++bankCount == 1) {
-                    materials = new String[bankData.length - 1];
-                    for (int i = 0; i < bankData.length - 1; i++) {
-                        materials[i] = bankData[i + 1];
-                    }
+                    materials = Arrays.asList(bankData).subList(3, bankData.length).stream().map(String::toLowerCase).collect(Collectors.toList());
                 } else {
-                    Bank bank = new Bank(bankData[0]);
+                    Bank bank = new Bank(bankData[0],
+                            new Location(Double.parseDouble(bankData[2]), Double.parseDouble(bankData[1])));
                     bankSet.add(bank);
 
-                    assert(bankData.length == materials.length - 1);
+                    assert(bankData.length == materials.size() - dataOffset);
 
-                    for (int i = 0; i < materials.length; i++) {
-                        bank.addMaterial(materials[i], bankData[i + 1].equals("1"));
+                    for (int i = 0; i < materials.size(); i++) {
+                        bank.addMaterial(materials.get(i), bankData[i + dataOffset].equals("1"));
                     }
                 }
             }
@@ -43,35 +40,78 @@ public class Banks {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if (br != null) {
+            if (reader != null) {
                 try {
-                    br.close();
+                    reader.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
+    }
 
+    public Bank queryMaterial(String material, Location userLocation) { //returns null if cannot recycle
+        if (!materials.contains(material.toLowerCase())) {
+            System.err.println("Material " + material + " not found in recycling data");
+            return null;
+        }
 
+        Set<Bank> validBanks = bankSet.stream().filter(bank -> bank.doesRecycle(material)).collect(Collectors.toSet());
+        if (validBanks.isEmpty()) {
+            return null;
+        }
+
+        double minDist = Double.MAX_VALUE;
+        Bank closestBank = null;
+
+        for (Bank bank : validBanks) {
+            double dist = bank.getLocation().getDistanceTo(userLocation);
+            if (dist < minDist) {
+                minDist = dist;
+                closestBank = bank;
+            }
+        }
+
+        return closestBank;
     }
 
     public static void main(String[] args) {
         String csvFile = "cam_data.csv";
-        new Banks(csvFile);
+        Banks banks = new Banks(csvFile);
+        Location userLocation = new Location("Pembroke College Cambridge");
+        Bank closestBank = banks.queryMaterial("glass", userLocation);
+        if (closestBank != null) {
+            System.out.println(closestBank.getName());
+        } else {
+            System.out.println("Cannot recycle");
+        }
     }
-
 }
 
 class Bank {
 
     private Map<String, Boolean> recyclables = new HashMap<>();
     private String name;
+    private Location location;
 
-    public Bank(String name) {
+    public Bank(String name, Location location) {
         this.name = name;
+        this.location = location;
     }
 
     public void addMaterial(String material, boolean recycles) {
-        recyclables.put(material, recycles);
+        recyclables.put(material.toLowerCase(), recycles);
+    }
+
+    public boolean doesRecycle(String material) {
+        return recyclables.get(material.toLowerCase());
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public String getName() {
+        return name;
     }
 }
---
+++ src/Location.java
@@ -0,0 +1,65 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Location {
+    private double longitude;
+    private double latitude;
+
+    private static final String GEOCODE_TOKEN = "893f62730e9a83";
+
+    private static final String GEOCODE_BASE_URL = "https://eu1.locationiq.com/v1/search.php?key=%s&q=%s&format=json";
+    private static final String PLACES_BASE_URL = "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest" + "?text=%s" + "&f=json";
+
+    private String GeocodeSearchUrl;
+
+    public Location(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public Location(String input) {
+
+        this.GeocodeSearchUrl = String.format(GEOCODE_BASE_URL, GEOCODE_TOKEN, input);
+
+        try {
+            URL url = new URL(GeocodeSearchUrl);
+            URLConnection request = url.openConnection();
+            request.connect();
+
+            JsonParser jp = new JsonParser(); //from gson
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
+            JsonArray rootArr = root.getAsJsonArray(); // convert json element to json array as the output gives an array of json objects
+            JsonObject rootObj = rootArr.get(0).getAsJsonObject(); // get the first json from the array and convert that to json object
+
+            latitude = rootObj.get("lat").getAsDouble();
+            longitude = rootObj.get("lon").getAsDouble();
+            //this.placeId = rootObj.get("place_id").getAsString();
+
+        } catch (Exception e) {
+            System.err.println("IOException: " + e.getMessage());
+            latitude = 52.2107375;
+            longitude = 0.09179849999999999;
+            //this.placeId = "333245852306";
+        }
+
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getDistanceTo(Location loc) {
+        return Math.sqrt(Math.pow(loc.longitude - longitude, 2) + Math.pow(loc.latitude, latitude));
+    }
+}
